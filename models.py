0b3a028cb144f3a48981b8127cf509334f9e0122
Lucas Nóbrega <lucasfernanndo07@gmail.com>
2025-08-13 09:41:04 -0300

    Criação da coluna email

--- models.py
+++ models.py
@@ -4,7 +4,8 @@ class User(db.Model):
     __tablename__ = 'usuarios'
     
     id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.String(40), unique=True, nullable=False)
+    username = db.Column(db.String(40), unique=True, nullable=False)
     nome = db.Column(db.String(40), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
     senha = db.Column(db.String(250), nullable=False)
     data_nascimento = db.Column(db.Date, nullable=False)
